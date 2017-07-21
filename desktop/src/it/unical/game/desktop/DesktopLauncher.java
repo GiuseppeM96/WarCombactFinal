baab4ab54fcea0e807f015a7f6a780427560fd37
GiuseppeM96 <giuseppemazzotta8@gmail.com>
2017-07-21 21:52:59 +0200

    icon

--- desktop/src/it/unical/game/desktop/DesktopLauncher.java
+++ desktop/src/it/unical/game/desktop/DesktopLauncher.java
@@ -1,14 +1,15 @@
 package it.unical.game.desktop; 
 
+import com.badlogic.gdx.Files;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
 import game.manager.WarCombat;
 
-
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.addIcon("src/img/icone.png", Files.FileType.Internal);
 		new LwjglApplication(new WarCombat(), config);
 	}
 }
